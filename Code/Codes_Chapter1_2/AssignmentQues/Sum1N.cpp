8829fc2020ab26fbde27881b2d8bd5fced36cc7c
gehendra <testhello008@gmail.com>
2026-09-09 22:24:33 +0545

    added code to sum1N.cpp

--- Code/Codes_Chapter1_2/AssignmentQues/Sum1N.cpp
+++ Code/Codes_Chapter1_2/AssignmentQues/Sum1N.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 
 int main() {
@@ -11,5 +12,21 @@ int main() {
     }
 
     std::cout << "Sum of numbers from 1 to " << n << " is: " << sum << "\n";
+    return 0;
+}
+*/
+
+#include <iostream>
+int main(){
+    int n, sub, fin = 0;
+    std::cout<<"if you enter the number will be subracted from 1 to n turn by turn and the result will be printed\n";
+    std::cout<<"enter a number from 1 to: ";
+    std::cin >> n;
+    std::cout<<"enter the number which you want to be subracted from: ";
+    std::cin>> sub;
+    for (int i=1; i<=n; i++){
+        fin = sub - i;
+    }
+    std:: cout<<"the number"<< sub<<" is subracted from 1 to "<< n << "\n And the result is "<<fin <<"\n";
     return 0;
 }
