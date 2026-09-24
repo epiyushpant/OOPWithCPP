a8368a3f3eb0e0e7e060d28be698cd5e104e1ee1
Muskan <muskankhadgi63@gmail.com>
2026-09-24 07:00:53 +0545

    new_file added

---
+++ Muskan/Untitled-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (isPrime(number))
+        std::cout << number << " is a prime number.\n";
+    else
+        std::cout << number << " is not a prime number.\n";
+
+    return 0;
+}
+
+
---
+++ Untitled-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+	int main(){
+		char a[15] = "JENISH";
+		
+		printf("%s",a);
+		
+		return 0;
+	}
